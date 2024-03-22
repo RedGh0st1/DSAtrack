c8f92bbe63c1106b5462920d78e187962c13f1d0
Lennie Nurse <lnursej@gmail.com>
2024-03-21 22:29:08 -0400

    fixed syntax error

--- hash_map_table/hash_map.js
+++ hash_map_table/hash_map.js
@@ -5,7 +5,7 @@ class HashMap {
   }
 
   hash(key) {
-    //1. loop through the key value 
+    //1. loop through the key value
     //2. using any hashing algorithm or function or you can simply make one up
     //3. it should return a position within the size of the array
     //4. since in this example the array size is 10
@@ -26,20 +26,21 @@ class HashMap {
     //1. check if the key already exists
     //create a new node and put it in the bucket based on given postion
     // handle the collision case
-    if(!this.buckets[index]){
-        let newHasHnode = new HasHNode(key,value)
-        this.buckets[index] =  newHasHNode;
-    }else{
-        let currentNode = this.buckets[index]
+    if (!this.buckets[index]) {
+      let newHasHnode = new HasHNode(key, value);
+      this.buckets[index] = newHasHnode;
+    } else {
+      let currentNode = this.buckets[index];
 
-        while(currentNode.next){
-            currentNode = currentNode.next
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = new HasHNode(key, value);
     }
-    currentNode.next = new HasHNode(key,value)
   }
 
-  showMap(){
-    return JSON.stringify(this.buckets)
+  showMap() {
+    return JSON.stringify(this.buckets);
   }
 }
 
@@ -51,5 +52,5 @@ class HasHNode {
   }
 }
 
-let newHashTable = new HashMap(10)
-console.log(newHashTable.insert("David","david@gmail.com");
+let newHashTable = new HashMap(10);
+console.log(newHashTable.insert("David", "david@gmail.com"));
