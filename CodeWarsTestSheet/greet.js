8a17848bec86e19d4dee31738b047b5f1665c52b
Lennie Nurse <lnursej@gmail.com>
2024-03-25 12:24:25 -0400

    8 kyu solved codewars problem

---
+++ CodeWarsTestSheet/greet.js
@@ -0,0 +1,53 @@
+function greet(language) {
+  let langData = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+  if (Object.keys(langData).includes(language))
+    return langData[language] || "Welcome. Try again.";
+}
+
+// another version
+// if (langData.hasOwnProperty(language)) {
+//     return langData[language];
+//   } else {
+//     return "Welcome"; // Return default value if language not found
+//   }
+
+// function greet(language) {
+// var greet = {
+//     english: 'Welcome',
+//     czech: 'Vitejte',
+//     danish: 'Velkomst',
+//     dutch: 'Welkom',
+//     estonian: 'Tere tulemast',
+//     finnish: 'Tervetuloa',
+//     flemish: 'Welgekomen',
+//     french: 'Bienvenue',
+//     german: 'Willkommen',
+//     irish: 'Failte',
+//     italian: 'Benvenuto',
+//     latvian: 'Gaidits',
+//     lithuanian: 'Laukiamas',
+//     polish: 'Witamy',
+//     spanish: 'Bienvenido',
+//     swedish: 'Valkommen',
+//     welsh: 'Croeso'
+//   };
+//   return greet[language] || 'Welcome';
+// }
