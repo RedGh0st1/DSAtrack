7a645f4fe80b9e699cb85c0d80da8edd5243e940
Lennie Nurse <lnursej@gmail.com>
2024-04-28 16:24:02 -0400

    codewar problems solved

---
+++ CodeWarsTestSheet/8Kyu/excludingVatPrice.js
@@ -0,0 +1,20 @@
+// Write a function that calculates the original product price, without VAT.
+
+// Example
+// If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
+
+// Thus, if your function receives 230.00 as input, it should return 200.00
+
+// Notes:
+
+// VAT is always 15% for the purposes of this Kata.
+// Round the result to 2 decimal places.
+// If null value given then return -1
+
+function excludingVatPrice(price) {
+  TotalPrice = Number(price / 1.15);
+  return price === null ? -1 : +TotalPrice.toFixed(2);
+}
+
+console.log(excludingVatPrice(230) === 200.0);
+console.log(excludingVatPrice(123) === 106.96);
