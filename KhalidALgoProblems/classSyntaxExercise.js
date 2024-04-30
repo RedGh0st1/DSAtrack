27110820f0b4714e738ebce68992ef1f5c3e6b96
Lennie Nurse <lnursej@gmail.com>
2024-04-30 18:37:55 -0400

    class and constructor pracitice

---
+++ KhalidALgoProblems/classSyntaxExercise.js
@@ -0,0 +1,18 @@
+class Car {
+  constructor() {
+    this.name = "camry";
+  }
+}
+
+const car = new Car();
+
+function House(location, size) {
+  this.location = location;
+  this.size = size;
+}
+const arr = [];
+console.log(globalThis.Array);
+const arr2 = new Array();
+House.prototype = { sayName };
+const home = new House("earth", 0);
+console.log(home.location);
---
+++ KhalidALgoProblems/constructorpracitice.js
@@ -0,0 +1,42 @@
+//write a constructor function for a computer
+// computer has
+// a screen width
+// a screen height
+// a home
+// have a function to print "hello world"
+
+// sudo
+// create a constructor function with three params
+// assign those params to keys on each instance of my computer
+// create a function to print hello world plus
+function Computer(screenWidth, screenHeight, name) {
+  this.name = name;
+  this.screenWidth = screenWidth;
+  this.screenHeight = screenHeight;
+
+//   this.print = ()=> {
+//     return "hello world";
+//   };
+
+//The prototype property of a constructor function in JavaScript is used to add methods or properties that will be shared by all instances created using that constructor function. 
+Computer.prototype.print = function.print(){
+    return "ha ha i printed this!!"
+}
+}
+
+// class and constructor functions are very similar
+class Computer{
+    constructor(width,height,name){
+        this.name=name;
+        this.width=width;
+        this.height=height;
+    }
+   print(){
+    return  `The ${this.name} is ${this.width} inches wide and ${this.height} inches tall.`
+   }
+}
+
+let myMacComputer = new Computer(27,19, macbook);
+let yourWindowsPC = new Computer(30,45,"windows PC");
+console.log(myMacComputer.print());
+console.log(yourWindowsPC.print());
