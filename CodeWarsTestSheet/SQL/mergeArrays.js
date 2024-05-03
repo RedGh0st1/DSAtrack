99db88f09adb9dbf58f2311ac85d88bd4bf3415a
Lennie Nurse <lnursej@gmail.com>
2024-05-03 10:48:25 -0400

    moved files to proper section

--- CodeWarsTestSheet/SQL/mergeArrays.js
+++
@@ -1,9 +0,0 @@
-// Write a function that merges two sorted arrays into a single one.
-// The arrays only contain integers.
-// Also, the final outcome must be sorted and not have any duplicate.
-
-function mergeArrays(a, b) {
-  const mergedArr = a.concat(b);
-  const uniqueArr = new Set(mergedArr);
-  return [...uniqueArr].sort((a, b) => a - b);
-}
