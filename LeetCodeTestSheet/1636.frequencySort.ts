64cac152f4f7274003b9f04cfdbcd6bc583c3284
Lennie Nurse <lnursej@gmail.com>
2024-06-04 11:48:30 -0400

    solved DSA problem

---
+++ LeetCodeTestSheet/1636.frequencySort.ts
@@ -0,0 +1,42 @@
+// Given an array of integers nums, sort the array in increasing order based on the frequency of the values.
+// If multiple values have the same frequency, sort them in decreasing order.
+
+// Return the sorted array.
+
+function frequencySort(nums: number[]): number[] {
+  let freqMap = new Map<number, number>();
+
+  //  finding the frequency using map/object
+  for (const num of nums) {
+    let key: number = num;
+    freqMap.set(key, (freqMap.get(key) || 0) + 1);
+  }
+
+  // sort by the frequency / value
+  const sortedValues: number[] = nums.sort((a: number, b: number): number => {
+    const freqMapA: number | undefined = freqMap.get(a);
+    const freqMapB: number | undefined = freqMap.get(b);
+
+    if (freqMapA === freqMapB) return b - a;
+    else return freqMapA - freqMapB;
+  });
+
+  return sortedValues;
+}
+
+// Example 1:
+console.log(frequencySort([1, 1, 2, 2, 2, 3]));
+// Input: nums = [1,1,2,2,2,3]
+// Output: [3,1,1,2,2,2]
+// Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
+
+// Example 2:
+console.log(frequencySort([2, 3, 1, 3, 2]));
+// Input: nums = [2,3,1,3,2]
+// Output: [1,3,3,2,2]
+// Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
+
+// Example 3:
+console.log(frequencySort([-1, 1, -6, 4, 5, -6, 1, 4, 1]));
+// Input: nums = [-1,1,-6,4,5,-6,1,4,1]
+// Output: [5,-1,4,4,-6,-6,1,1,1]
